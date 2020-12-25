2ec3a4ad84750b8805e5eabfbca7210d3b1d2eba
Matomatt <47554186+Matomatt@users.noreply.github.com>
2020-12-25 23:34:53 +0100

    Switch to godot engine

---
+++ player.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public class player : KinematicBride oride dy2D
+{
+    public int movespeed = 500;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+
+    }
+
+//    public override _
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//
+//  }
+}
