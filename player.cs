f88711c93f881bf283d8ea4b8ee83a9782bd44bc
Matomatt <47554186+Matomatt@users.noreply.github.com>
2020-12-26 11:09:41 +0100

    Raquette movement and dash

--- player.cs
+++ player.cs
@@ -1,9 +1,15 @@
 using Godot;
 using System;
+using System.Threading.Tasks;
 
-public class player : KinematicBride oride dy2D
+public class player : KinematicBody2D
 {
-    public int movespeed = 500;
+    [Export]
+    private int movespeed = 500;
+    private int lastspeed = 500;
+    [Export]
+    private float dashtime = 0.2f;
+    private bool dashing = false;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -11,11 +17,27 @@ public class player : KinematicBride oride dy2D
 
     }
 
-//    public override _
+    public override void _PhysicsProcess(float delta)
+    {
+        if (Input.IsActionPressed("quit")) GetTree().Quit();
+        if (Input.IsActionPressed("spacebar") && !dashing)
+        {
+            dashing = true;
+            lastspeed = movespeed;
+            movespeed *= 3;
+            DashTimeout(dashtime);
+        }
+        Vector2 movement = new Vector2(
+            Input.GetActionStrength("moveright") - Input.GetActionStrength("moveleft"),
+            Input.GetActionStrength("movedown") - Input.GetActionStrength("moveup")
+        );
+        MoveAndCollide(movement.Normalized() * movespeed * delta);
+    }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//
-//  }
+    private async void DashTimeout(float time)
+    {
+        await Task.Delay(TimeSpan.FromMilliseconds(time * 1000f));
+        movespeed = lastspeed;
+        dashing = false;
+    }
 }
