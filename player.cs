650a908305ab7fb23312abb0283373e5475fcd70
Matomatt <47554186+Matomatt@users.noreply.github.com>
2020-12-30 22:34:21 +0100

    + touchscreen support

--- player.cs
+++ player.cs
@@ -30,9 +30,20 @@ public class player : KinematicBody2D
 	private DateTime lastDash = DateTime.Now;
 	private CPUParticles2D dashParticles;
 	private Timer particlesTimer;
+	[Export]
+	private bool touchControls = false;
+	[Export]
+	private int touchRadius = 30;
+	[Export]
+	private int dashDistanceThreshold = 50;
+	private int touchedByID = -1;
+	private bool hovered = false;
+	private Vector2 targetPosition;
+	private int lastDistanceBetweenDrag = 0;
 
 	public override void _Ready()
 	{
+		InputPickable = true;
 		pn = playerNumber.ToString();
 		(sprite = GetChild<AnimatedSprite>(0)).FlipH = flip;
 		sprite.Connect("animation_finished", this, nameof(Idle));
@@ -43,28 +54,92 @@ public class player : KinematicBody2D
 		if (flip) { dashParticles.Gravity *= -1; dashParticles.Direction *= -1; }
 		(dashTimer = (Timer)GetNode("dashTimer")).Connect("timeout", this, nameof(DashTimeout));
 		(particlesTimer = (Timer)GetNode("particlesTimer")).Connect("timeout", this, nameof(ResetDashParticles));
+		Connect("mouse_entered", this, nameof(Hovered));
+		Connect("mouse_exited", this, nameof(Hovered));
 	}
 
-    public override void _PhysicsProcess(float delta)
+	public override void _PhysicsProcess(float delta)
 	{
 		if (lastDash.CompareTo(DateTime.Now) > 0) dashParticles.Emitting = false;
 		else if (!dashParticles.Emitting) dashParticles.Emitting = true;
-		if (Input.IsActionPressed("dash" + pn) && !dashing && lastDash < DateTime.Now) Dash();
 
-		Vector2 speedGoal = new Vector2(
-			Input.GetActionStrength("moveright" + pn) - Input.GetActionStrength("moveleft" + pn),
-			Input.GetActionStrength("movedown" + pn) - Input.GetActionStrength("moveup" + pn)
-		).Normalized();
-		speedGoal = ((!dashing)? speedGoal : direction) * moveSpeed;
+		Vector2 speedGoal = touchedByID >= 0 ? TouchToSpeedGoal() : InputsToSpeedGoal();
+		speedGoal = ((!dashing) ? speedGoal : direction) * moveSpeed;
 		playerSpeed = playerSpeed.LinearInterpolate(speedGoal, acceleration * delta);
 		MoveAndSlide(playerSpeed);
 		for (int i = 0; i < GetSlideCount(); i++)
-        {
+		{
 			string colliderName = ((Node2D)GetSlideCollision(i).Collider).Name;
 			if (colliderName == "ball")
-				((ball)GetSlideCollision(i).Collider).collideWith(this, delta, -1*GetSlideCollision(i).Normal);
+				((ball)GetSlideCollision(i).Collider).collideWith(this, delta, -1 * GetSlideCollision(i).Normal);
 		}
 	}
+
+    public override void _Input(InputEvent @event)
+    {
+		if (@event.IsActionPressed("dash" + pn) && !dashing && lastDash < DateTime.Now) Dash(true);
+
+		if (@event is InputEventScreenTouch screenTouch)
+        {
+			if (screenTouch.Pressed && screenTouch.Position.DistanceTo(GlobalPosition) < touchRadius)
+            {
+				//GD.Print(pn + " : " + screenTouch.Index);
+				targetPosition = screenTouch.Position;
+				touchedByID = screenTouch.Index;
+			}
+				
+			else if (touchedByID == screenTouch.Index)
+            {
+				//GD.Print("released");
+				touchedByID = -1;
+            }
+		} else if (@event is InputEventScreenDrag screenDrag)
+        {
+			if (screenDrag.Index == touchedByID)
+            {
+				
+				lastDistanceBetweenDrag = (int)screenDrag.Position.DistanceTo(targetPosition);
+				//GD.Print(screenDrag.Position.DistanceTo(targetPosition));
+				if (lastDistanceBetweenDrag > dashDistanceThreshold && !dashing && lastDash < DateTime.Now)
+                {
+					direction = new Vector2(screenDrag.Position.x - targetPosition.x, screenDrag.Position.y - targetPosition.y).Normalized();
+					Dash(false);
+                }
+					
+				targetPosition = screenDrag.Position;
+			}
+				
+		}
+			
+	}
+
+    private Vector2 InputsToSpeedGoal()
+	{
+		return new Vector2(
+			Input.GetActionStrength("moveright" + pn) - Input.GetActionStrength("moveleft" + pn),
+			Input.GetActionStrength("movedown" + pn) - Input.GetActionStrength("moveup" + pn)
+		).Normalized();
+	}
+
+	private Vector2 TouchToSpeedGoal()
+	{
+		return new Vector2(targetPosition.x - GlobalPosition.x, targetPosition.y - GlobalPosition.y).Normalized();// *(lastDistanceBetweenDrag/dashDistanceThreshold);
+	}
+
+	private void Hovered()
+    {
+		hovered = !hovered;
+		GD.Print("Hovered " + hovered.ToString() );
+    }
+
+	/*private void Touched()
+    {
+		if (hovered)
+        {
+			touchedByID = 1;
+		}
+			
+	}*/
 
 	internal Vector2 getSpeed()
 	{
@@ -80,12 +155,12 @@ public class player : KinematicBody2D
 		sprite.Play("idle");
 	}
 
-	private void Dash()
+	private void Dash(bool keyboardInitiated)
     {
 		dashing = true;
 		lastSpeed = moveSpeed;
         moveSpeed = (int)(moveSpeed * dashSpeedMultiplier);
-		direction = playerSpeed.Normalized();
+		if (keyboardInitiated) direction = playerSpeed.Normalized();
 		BurstDashParticles();
 		dashTimer.Start(dashTime);
 		particlesTimer.Start(dashTime + 0.5f);
