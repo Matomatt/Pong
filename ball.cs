fd7512f07ae5a1e394656517b5ad9c40fd86f5fe
Matomatt <47554186+Matomatt@users.noreply.github.com>
2021-01-06 13:06:37 +0100

    ~ Fix ball collisions with the rackets + Explosion animation and sound when the ball get stuck

--- ball.cs
+++ ball.cs
@@ -9,7 +9,7 @@ public class ball : KinematicBody2D
     const int MAXSPEED = 800;
     private Vector2 speed;
     private DateTime[] latestCollide = { DateTime.Now, DateTime.Now };
-    const float TIMEBETWEENCOLLISIONCALCULATIONS = 0.1f;
+    const float TIMEBETWEENCOLLISIONCALCULATIONS = 0.05f;
     public float speedModifier = 1f;
     [Export]
     private float timeBeforeUnblocking = 1f;
@@ -23,7 +23,7 @@ public class ball : KinematicBody2D
     {
         GD.Randomize();
         resetSpeed();
-
+        ((AnimatedSprite)GetNode("../explosion")).Connect("animation_finished", this, nameof(ExplosionEnded));
     }
 
     private void resetSpeed()
@@ -38,7 +38,7 @@ public class ball : KinematicBody2D
 
         KinematicCollision2D collision = MoveAndCollide(speed * delta * speedModifier, infiniteInertia: false);
 
-        if (collision != null)
+        if (collision != null && latestCollide[0].CompareTo(DateTime.Now) < 0)
         {
             speed = speed.Bounce(collision.Normal);
             string colliderName = ((Node2D)collision.Collider).Name;
@@ -50,21 +50,20 @@ public class ball : KinematicBody2D
                 GlobalPosition = new Vector2(0, 0);
                 ((mainscene)GetNode("/root/mainscene")).Goal(colliderName);
             }
+            latestCollide[0] = DateTime.Now.AddSeconds(TIMEBETWEENCOLLISIONCALCULATIONS);
         }
 
-        if (lastPosition.DistanceTo(GlobalPosition)>0.5) blockingStartedAt = DateTime.Now;
-        GD.Print(lastPosition.DistanceTo(GlobalPosition));
-        //GD.Print(touchingBall.ToString() + " + " + (int)lastPosition.x + " == " + (int)GlobalPosition.x + " && " + ((int)lastPosition.y == (int)GlobalPosition.y) + " - " + blockingStartedAt);
+        if (lastPosition.DistanceTo(GlobalPosition) > 0.5) blockingStartedAt = DateTime.Now;
+
         if (lastPosition.DistanceTo(GlobalPosition) <= 0.5 && blockingStartedAt.AddSeconds(timeBeforeUnblocking) < DateTime.Now)
         {
             GD.Print("BOOM");
+            ((AnimatedSprite)GetNode("../explosion")).GlobalPosition = GlobalPosition + new Vector2(256, 128);
+            ((AnimatedSprite)GetNode("../explosion")).Play("boom");
+            ((globalVariables)GetNode("/root/GlobalVariables")).PlaySfx("explosion.wav");
             player[] rackets = ((mainscene)GetNode("/root/mainscene")).rackets;
             for (int i = 0; i < rackets.Length; i++)
-            {
-                GD.Print(rackets[i].GlobalPosition.x + " - " + GlobalPosition.x);
                 rackets[i].ApplyForce(new Vector2(rackets[i].GlobalPosition.x - GlobalPosition.x-256, rackets[i].GlobalPosition.y - GlobalPosition.y-128).Normalized());
-            }
-                
         }
         samplePositionTime -= delta;
         if (samplePositionTime <= 0)
@@ -77,14 +76,15 @@ public class ball : KinematicBody2D
         
     internal void collideWith(player raquette, float delta, Vector2 normal)
     {
+        //((globalVariables)GetNode("/root/GlobalVariables")).PlaySfx("ping" + ((int)GD.RandRange(0, 5)).ToString()+".wav");
         normal = new Vector2(Math.Abs(normal.x), Math.Abs(normal.y));
-        if (latestCollide[raquette.Name[raquette.Name.Length - 1] - '1'].CompareTo(DateTime.Now) < 0)
-        {
-            speed += speed * SPEEDMULTIPLIER * delta + normal * raquette.getSpeed();
-            if (speed.Length() > MAXSPEED) speed = speed.Normalized() * MAXSPEED;
-            latestCollide[raquette.Name[raquette.Name.Length - 1] - '1'] = DateTime.Now.AddSeconds(TIMEBETWEENCOLLISIONCALCULATIONS);
-            raquette.Ping();
-        }
+        speed += speed * SPEEDMULTIPLIER * delta + normal * raquette.getSpeed();
+        if (speed.Length() > MAXSPEED) speed = speed.Normalized() * MAXSPEED;
+        raquette.Ping();
+    }
 
+    private void ExplosionEnded()
+    {
+        ((AnimatedSprite)GetNode("../explosion")).Play("default");
     }
 }
